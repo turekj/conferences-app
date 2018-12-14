3d9fd22b81b4e2d57ff30c851c2d15e949d3f84d
Jakub Turek <jkbturek@gmail.com>
2018-12-14 17:04:58 +0100

    Remove unused code

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  ConferencesApp
-//
-//  Created by Jakub Turek on 14/12/2018.
-//  Copyright © 2018 Jakub Turek. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -18,11 +10,4 @@ class ViewController: UIViewController {
         return nil
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
-

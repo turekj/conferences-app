73dd0f2371bc3567ca3d9e9d6e4de8ab726e44c9
Jakub Turek <jkbturek@gmail.com>
2018-12-14 16:22:35 +0100

    Add default controller for ViewController

--- ConferencesApp/ViewController.swift
+++ ConferencesApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

8cb317255cc8f0cf512bf55b61723ff510efb845
Jakub Turek <jkbturek@gmail.com>
2018-12-14 13:58:00 +0100

    Add empty Xcode project

---
+++ ConferencesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConferencesApp
+//
+//  Created by Jakub Turek on 14/12/2018.
+//  Copyright © 2018 Jakub Turek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

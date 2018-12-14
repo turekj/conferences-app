82e072ed2e418c4e3aa8c627e0470a44631dedc4
Jakub Turek <jkbturek@gmail.com>
2018-12-14 17:03:25 +0100

    Remove launch options variable

--- Sources/UI/Application/AppDelegate.swift
+++ Sources/UI/Application/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
             |> root(Current.navigation.controller)
             <> { $0.makeKeyAndVisible() }
